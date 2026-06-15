d7cb9177c4fc64678e61c560d005ec0b24ba3942
Shadow8021 <gregoyaga@gmail.com>
2026-06-15 18:10:53 +0100

    text widget Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Image.network(
+        "https://static.vecteezy.com/system/resources/previews/025/463/773/non_2x/hacker-logo-design-a-mysterious-and-dangerous-hacker-illustration-vector.jpg",
+      ),
+    ),
+  );
+}
